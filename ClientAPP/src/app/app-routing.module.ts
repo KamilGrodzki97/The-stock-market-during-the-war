31384cd6b3e0d76b217b7ebc5f5ae59a8002f6d8
KamilGrodzki97 <kam.gro@o2.pl>
2022-02-26 23:50:20 +0100

    Angular-material header

--- ClientAPP/src/app/app-routing.module.ts
+++ ClientAPP/src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    //{path: '', component: COMPONENTNAME},
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- ClientAPP/src/app/app.module.ts
+++ ClientAPP/src/app/app.module.ts
@@ -5,18 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot([
-      //{path: '', component: COMPONENTNAME},
-    ]),
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
